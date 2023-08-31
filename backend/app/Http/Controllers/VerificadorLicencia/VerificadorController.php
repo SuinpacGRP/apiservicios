9b7f9f64aba40376b35604f324a95d88ecf7168e
SUINPAC GRP <contacto@suinpac.com>
2023-08-31 12:20:22 -0600

    irvindev - Modificacion en la consulta en la direccion

--- backend/app/Http/Controllers/VerificadorLicencia/VerificadorController.php
+++ backend/app/Http/Controllers/VerificadorLicencia/VerificadorController.php
@@ -31,7 +31,7 @@ class VerificadorController extends Controller{
         $Respuesta = '';
         $ConsultaLicencia = Funciones::ObtenValor("SELECT l.id,l.FolioLicencia AS Folio , (SELECT CONCAT_WS(' - ', Tipo, NOMBRE) AS Tipo FROM TipoLicenciaManejo WHERE  id = l.TipoLicencia) AS Tipo,
             c.Nombres, CONCAT_WS(' ', c.ApellidoPaterno, c.ApellidoMaterno) AS Apellidos, c.Curp, l.FechaFinal AS Vence, l.FechaInicial, d.Extra_Donante AS Donante,
-            d.Extra_TipoSangre AS TipoSangre, CONCAT('Calle ', c.Calle_c, ' Nº Int ', c.N_umeroInterior_c, ' Colonia ', c.Colonia_c, '. C.P. ', C_odigoPostal_c, ' ',
+            d.Extra_TipoSangre AS TipoSangre, CONCAT('Calle ', c.Calle_c, ' Nº Int ', COALESCE(c.N_umeroInterior_c, 'S/N'), ' Colonia ', c.Colonia_c, '. C.P. ', C_odigoPostal_c, ' ',
             (SELECT Nombre FROM Municipio where id = c.Municipio_c), ', ', (SELECT Nombre FROM EntidadFederativa WHERE id = c.EntidadFederativa_c) ) AS Direccion,
             TIMESTAMPDIFF(YEAR, l.FechaInicial, l.FechaFinal) AS Vigencia, l.FotoFrente, d.Extra_ContactoEmergNombre AS NombreContacto,
             d.Extra_ContactoEmergTelefono AS TelefonoContacto, d.Extra_ContactoEmergDireccion AS DireccionContacto
